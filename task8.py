ed6f25ede32732ddaed621d8dbe6b542590112e8
fata9ty <fata9ty@gmail.com>
2023-12-17 12:06:06 +0300

    task8 actually completed

--- task8.py
+++ task8.py
@@ -2,7 +2,6 @@ import logging
 import re
 from secrets import TOKEN
 import datetime
-import time
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
@@ -62,12 +61,13 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-SET_NAME, SET_DATE, SET_TIME, SET_DETAILS, SET_DELETE = map(chr, range(5))
-
+(SET_EVENT, SET_NAME, SET_DATE, SET_TIME, SET_DETAILS, EDIT_EVENT, EDIT_NAME, EDIT_DATE, EDIT_TIME,
+ EDIT_DETAILS, DELETE) = map(chr, range(11))
+END = ConversationHandler.END
 
 def start(update, context):
     try:
-        reply_markup = ReplyKeyboardMarkup([['/set', '/list'], ['/delete']])
+        reply_markup = ReplyKeyboardMarkup([['/set', '/list'], ['/edit', '/delete']])
         update.message.reply_text("Что вы намереваетесь сделать?", reply_markup=reply_markup)
     except:
         print("Произошла ошибка в функции start.")
@@ -121,12 +121,77 @@ def time_question(update, context):
                 event_details=context.user_data['details']
             )
             update.message.reply_text("Событие успешно создано")
-            return ConversationHandler.END
+            return END
         else:
             update.message.reply_text("Время указано в неправильном формате")
             return SET_TIME
     except:
         print("Произошла ошибка в функции time_question.")
+
+
+def edit_event_handler(update, context):
+    try:
+        list_of_events(update, context)
+        update.message.reply_text("Введите номер изменяемого события")
+        return EDIT_EVENT
+    except:
+        print("Произошла ошибка в функции edit_event_handler.")
+def edit_event(update, context):
+    try:
+        context.user_data['id'] = int(update.message.text)
+        update.message.reply_text("Введите новое название события")
+        return EDIT_NAME
+    except:
+        print("Произошла ошибка в функции edit_event.")
+
+def edit_name(update, context):
+    try:
+        context.user_data['name'] = update.message.text
+        update.message.reply_text("Введите новое описание события")
+        return EDIT_DETAILS
+    except:
+        print("Произошла ошибка в функции edit_name.")
+
+def edit_details(update, context):
+    try:
+        context.user_data['details'] = update.message.text
+        update.message.reply_text("Введите новую дату в формате ДД-ММ-ГГГГ")
+        return EDIT_DATE
+    except:
+        print("Произошла ошибка в функции edit_details.")
+
+def edit_date_question(update, context):
+    try:
+        user_input = update.message.text.strip()
+        if re.match(r'^\d{2}-\d{2}-\d{4}$', user_input):
+            context.user_data['date'] = user_input
+            update.message.reply_text("Введите новое время в формате ЧЧ:ММ")
+            return EDIT_TIME
+        else:
+            update.message.reply_text("Неправильный формат даты, введите дату в формате: ДД-ММ-ГГГГ")
+            return EDIT_DATE
+    except:
+         print("Произошла ошибка в функции edit_date_question.")
+
+def edit_time_question(update, context):
+    try:
+        user_input = update.message.text.strip()
+        if re.match(r'^\d{2}:\d{2}$', user_input):
+            time_str = user_input
+            calendar.edit_event(
+                event_id=context.user_data['id'],
+                event_name=context.user_data['name'],
+                event_date=context.user_data['date'],
+                event_time=time_str,
+                event_details=context.user_data['details']
+            )
+            update.message.reply_text("Событие успешно отредактировано")
+            return END
+        else:
+            update.message.reply_text("Время указано в неправильном формате")
+            return SET_TIME
+    except:
+        print("Произошла ошибка в функции edit_time_question.")
 
 def list_of_events(update, context):
     try:
@@ -137,20 +202,26 @@ def list_of_events(update, context):
                   f'Дата события: {calendar.events[event]["date"]}\n'
                   f'Время события: {calendar.events[event]["time"]}\n'
                   f'Описание события: {calendar.events[event]["details"]}\n')
-            return ConversationHandler.END
+            return END
     except:
         print("Произошла ошибка в функции list_of_events.")
 
 def delete_events(update, context):
-    list_of_events(update, context)
-    update.message.reply_text("Введите номер удаляемого события")
-    return SET_DELETE
+    try:
+        list_of_events(update, context)
+        update.message.reply_text("Введите номер удаляемого события")
+        return DELETE
+    except:
+        print("Произошла ошибка в функции delete_events.")
 
 def delete_handler(update, context):
-    selected_event = update.message.text
-    calendar.delete_event(selected_event)
-    update.message.reply_text("Событие успешно удалено")
-    return ConversationHandler.END
+    try:
+        selected_event = update.message.text
+        calendar.delete_event(selected_event)
+        update.message.reply_text("Событие успешно удалено")
+        return END
+    except:
+        print("Произошла ошибка в функции delete_handler.")
 
 
 def main():
@@ -164,16 +235,37 @@ def main():
             SET_DETAILS: [MessageHandler(Filters.text, details)],
             SET_DATE: [MessageHandler(Filters.text, date_question)],
             SET_TIME: [MessageHandler(Filters.text, time_question)],
-            SET_DELETE: [MessageHandler(Filters.text, delete_handler)]
         },
         fallbacks=[],
         allow_reentry=True
     )
-
+    delete_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('delete', delete_events)],
+        states={
+            DELETE: [MessageHandler(Filters.text, delete_handler)]
+        },
+        fallbacks=[],
+        allow_reentry=True
+    )
+    edit_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('edit', edit_event_handler)],
+        states={
+            EDIT_EVENT: [MessageHandler(Filters.text, edit_event)],
+            EDIT_NAME: [MessageHandler(Filters.text, edit_name)],
+            EDIT_DETAILS: [MessageHandler(Filters.text, edit_details)],
+            EDIT_DATE: [MessageHandler(Filters.text, edit_date_question)],
+            EDIT_TIME: [MessageHandler(Filters.text, edit_time_question)]
+        },
+        fallbacks=[],
+        allow_reentry=True
+    )
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('list', list_of_events))
     dispatcher.add_handler(CommandHandler('delete', delete_events))
+    dispatcher.add_handler(CommandHandler('edit', edit_event_handler))
     dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(edit_conv_handler)
+    dispatcher.add_handler(delete_conv_handler)
 
     updater.start_polling()
     updater.idle()
